34b6cd77933950211ae42c3d6b6e3a85db498e06
Jefferson Wu <makerjeffwu@gmail.com>
2016-02-12 12:12:47 -0800

    added 'manual draw maps' test, isolating out twitter

--- README.md
+++ README.md
@@ -3,17 +3,12 @@ Working with Google APIs with Node JS <br>
 Including Maps API <br>
 
 ### Based on these tutorials:
-[Node.JS Quick Start: Drive REST API](https://developers.google.com/drive/v3/web/quickstart/nodejs)
-
-[Google Maps Examples Jackpot](https://developers.google.com/maps/documentation/javascript/examples/)
-
-[Google Maps Tutorials](https://developers.google.com/maps/tutorials/)
-
-[Google Maps Javascript API](https://developers.google.com/maps/documentation/javascript/tutorial)
-
-[Maps Markers](https://developers.google.com/maps/documentation/javascript/markers)
-
-[Draw Library](https://developers.google.com/maps/documentation/javascript/drawinglayer)
+- [Node.JS Quick Start: Drive REST API](https://developers.google.com/drive/v3/web/quickstart/nodejs)
+- [Google Maps Examples Jackpot](https://developers.google.com/maps/documentation/javascript/examples/)
+- [Google Maps Tutorials](https://developers.google.com/maps/tutorials/)
+- [Google Maps Javascript API](https://developers.google.com/maps/documentation/javascript/tutorial)
+- [Google Maps Markers](https://developers.google.com/maps/documentation/javascript/markers)
+- [Google Maps Drawing Layer Library](https://developers.google.com/maps/documentation/javascript/drawinglayer)
 
 
 ## Notes
--- public/js/titleSwapper.js
+++ public/js/titleSwapper.js
@@ -3,14 +3,48 @@
  * TitleSwapper: Swaps tab titles based on focus and blur.
  */
 
-var siteTitle = 'SXSW 180';
+//var siteTitle = 'SXSW 180';
+//
+////when user switches to another tab or opens a link
+//window.addEventListener('blur', function(event){
+//    document.title = 'come back!';
+//});
+//
+////when user switches back to the tab
+//window.addEventListener('focus', function(event){
+//    document.title = siteTitle;
+//});
 
-//when user switches to another tab or opens a link
-window.addEventListener('blur', function(event){
-    document.title = 'come back!';
-});
 
-//when user switches back to the tab
-window.addEventListener('focus', function(event){
-    document.title = siteTitle;
-});
+// namespacing
+var JeffApp = {
+
+        switchTitle: function(focusTitle, blurTitle){
+            window.addEventListener('focus', function(event){
+                document.title = focusTitle;
+            });
+
+            window.addEventListener('blur', function(event){
+                document.title = blurTitle;
+            });
+        }
+    };
+
+
+var JeffApp2 = JeffApp2 || function(focusTitle, blurTitle) {
+    this.focusTitle = focusTitle;
+    this.blurTitle = blurTitle;
+};
+
+/**
+ *
+ * @type {Function|(function(this:*))}
+ */
+JeffApp2.prototype.switchToFocus = function(){
+    document.title = this.focusTitle;
+}.bind(this); //binds to parent object namespace
+
+JeffApp2.prototype.switchToBlur = function(){
+    document.title = this.blurTitle;
+}.bind(this);
+
